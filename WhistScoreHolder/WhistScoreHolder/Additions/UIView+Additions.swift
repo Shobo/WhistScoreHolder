2402ab91d56c9fa429e8e08bd712a105047b9c0e
Mihai Costiug <mihai.costiug@telenav.com>
2016-05-07 23:32:22 +0300

    Added logic for dynamically populating a view with a list of subviews. Will be used for betting options.

--- WhistScoreHolder/WhistScoreHolder/Additions/UIView+Additions.swift
+++ WhistScoreHolder/WhistScoreHolder/Additions/UIView+Additions.swift
@@ -11,6 +11,8 @@ import UIKit
 let kMargin: CGFloat = 6.0
 let kMinRowHeight: CGFloat = 80.0
 
+let kChoiceButtonsMargin: CGFloat = 25.0
+
 extension UIView {
     class func loadFromNibNamed(nibNamed: String, bundle : NSBundle? = nil) -> UIView! {
         return UINib(
@@ -18,4 +20,46 @@ extension UIView {
             bundle: bundle
             ).instantiateWithOwner(nil, options: nil)[0] as! UIView
     }
+    
+    func populateWithEqualSizedViews(views: [UIView]) { //views should be already styled, having labels and targets
+        self.removeSubviews()
+        
+        let numberOfViewsInLine = smallestSquareRootWithSquareLargerThan(views.count)
+        
+        let choiceButtonSize = (self.frame.width - CGFloat(numberOfViewsInLine + 1) * kChoiceButtonsMargin) / CGFloat(numberOfViewsInLine)
+        
+        var currentX: CGFloat = kChoiceButtonsMargin
+        var currentY: CGFloat = kChoiceButtonsMargin
+        
+        var itemsOnCurrentRow = 0
+        var currentRow = 1
+        
+        for view in views {
+            view.frame = CGRectMake(currentX, currentY, choiceButtonSize, choiceButtonSize)
+            self.addSubview(view)
+            
+            itemsOnCurrentRow += 1
+            
+            if itemsOnCurrentRow != numberOfViewsInLine {   //same row
+                currentX = view.frame.maxX + kChoiceButtonsMargin
+            } else {    //switch to next row
+                currentRow += 1
+                itemsOnCurrentRow = 0
+                
+                if currentRow == 0 {    //if last row; CHANGE CONDITION
+                    //special logic for last row
+                } else {
+                    currentX = kChoiceButtonsMargin
+                    currentY = view.frame.maxY + kChoiceButtonsMargin
+                }
+            }
+        }
+    }
+    
+    func removeSubviews() {
+        for subview in self.subviews {
+            subview.removeFromSuperview()
+        }
+    }
+    
 }
---
+++ WhistScoreHolder/WhistScoreHolder/Additions/WSHMathAdditions.swift
@@ -0,0 +1,31 @@
+//
+//  WSHMathAdditions.swift
+//  WhistScoreHolder
+//
+//  Created by Mihai Costiug on 07/05/16.
+//  Copyright © 2016 WSHGmbH. All rights reserved.
+//
+
+import Foundation
+
+func smallestSquareRootWithSquareLargerThan(number: Int) -> Int {
+    var counter = number
+    
+    while counter <= (number ^^ 2) {
+        let squareRoot = sqrt(Double(counter))
+        
+        if squareRoot == floor(squareRoot) {    //square root is an integer
+            return Int(squareRoot)
+        }
+        
+        counter += 1
+    }
+    
+    return 0    //no result found
+}
+
+infix operator ^^ { }
+
+func ^^(radix: Int, power: Int) -> Int {
+    return Int(pow(Double(radix), Double(power)))
+}
